f86c5f6cc0a3c2ef76047ce82f4c0f3ae3d9b489
Aone Shrestha <aoneshrestha58@gmail.com>
2024-04-12 05:52:41 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 
 function App() {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const handleToggle = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar
+        bg="dark"
+        data-bs-theme="dark"
+        expand="sm"
+        className="bg-body-tertiary"
+      >
+        <Container>
+          <Navbar.Brand href="#home">X</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Profile</Nav.Link>
+              <Nav.Link href="#link">Login</Nav.Link>
+
+              <NavDropdown title="User" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Log Out</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Routes>
+        {/* <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/" element={<Landing isAuth={isAuth} />} />
+      <Route path="/posts" element={<Posts isAuth={isAuth} isAdmin={isAdmin} />}/>
+      <Route path="/view" element={<ViewPost />} /> */}
+      </Routes>
+    </>
   );
 }
 
