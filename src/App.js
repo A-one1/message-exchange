82fe42a4f5395353263e448b77886649e7cce612
Aone Shrestha <aoneshrestha58@gmail.com>
2024-04-12 07:12:12 -0500

    basic login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import { useState } from "react";
-import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
-import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 
+import React from "react";
+import { useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import "./App.css";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import Login from "./pages/login";
+
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 function App() {
   const [isCollapsed, setIsCollapsed] = useState(true);
 
@@ -25,7 +31,7 @@ function App() {
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#link">Profile</Nav.Link>
-              <Nav.Link href="#link">Login</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
 
               <NavDropdown title="User" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Log Out</NavDropdown.Item>
@@ -35,6 +41,9 @@ function App() {
         </Container>
       </Navbar>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<App />} />
+
         {/* <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/" element={<Landing isAuth={isAuth} />} />
---
+++ src/pages/login.js
@@ -0,0 +1,244 @@
+import React, { useState } from "react";
+import { auth, db, provider } from "../firebase";
+import { useNavigate } from "react-router-dom";
+import {
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  query,
+  serverTimestamp,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { toast } from "react-toastify";
+import "../App.css";
+
+function Login({ setIsAuth }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const postCollectionRef = collection(db, "Users");
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then(async (userCredential) => {
+        setIsAuth(true);
+        localStorage.setItem("isAuth", true);
+
+        const name = auth.currentUser.displayName;
+        const email = auth.currentUser.email;
+        const id = auth.currentUser.uid;
+        const date = serverTimestamp();
+        const uid = auth.currentUser.uid;
+        localStorage.setItem("uid", uid);
+
+        const querySnapshot = await getDocs(
+          query(postCollectionRef, where("email", "==", email))
+        );
+        if (querySnapshot.size > 0) {
+          const docRef = doc(postCollectionRef, querySnapshot.docs[0].id);
+          await updateDoc(docRef, {
+            id,
+            date,
+            name,
+            email,
+          });
+        } else {
+          await addDoc(postCollectionRef, {
+            id,
+            date,
+            name,
+            email,
+            isAdmin: false,
+            isApproved: false,
+          });
+        }
+
+        navigate("/posts");
+      })
+      .catch((error) => {
+        toast.error(error.code.replace("auth/", ""));
+      });
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+        console.log(userCredential);
+        if (!userCredential.user.emailVerified) {
+          toast.error("Unverified user. Please check your email.");
+          sendEmailVerification(auth.currentUser);
+        } else {
+          setIsAuth(true);
+          localStorage.setItem("isAuth", true);
+          localStorage.setItem("uid", userCredential.user.uid);
+          navigate("/posts");
+        }
+
+        const date = serverTimestamp();
+
+        const querySnapshot = await getDocs(
+          query(postCollectionRef, where("email", "==", email))
+        );
+        if (querySnapshot.size > 0) {
+          const docRef = doc(postCollectionRef, querySnapshot.docs[0].id);
+          await updateDoc(docRef, {
+            date,
+          });
+        }
+      })
+      .catch((error) => {
+        toast.error(error.code.replace("auth/", ""));
+      });
+  };
+
+  const resetPass = () => {
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        toast.info("Password reset email sent.", {
+          position: toast.POSITION.TOP_CENTER,
+          autoClose: 3000,
+          theme: "colored",
+          hideProgressBar: true,
+          closeOnClick: true,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.code == "auth/missing-email") {
+          const emailInput = document.getElementById("email");
+          if (emailInput) {
+            emailInput.focus();
+            toast.error("Please input your email.", {
+              position: toast.POSITION.TOP_CENTER,
+              autoClose: 3000,
+              theme: "colored",
+              hideProgressBar: true,
+              closeOnClick: true,
+            });
+            return;
+          }
+        }
+      });
+  };
+
+  return (
+    <div className="loginPage">
+      <section className="signup">
+        <div className="container">
+          <div className="signin-content">
+
+            <div className="signin-form">
+              <h2 className="form-title">Login</h2>
+              <form className="register-form" id="login-form">
+                <div className="form-group">
+                  <label htmlFor="your_name">
+                    <i className="zmdi zmdi-account material-icons-name"></i>
+                  </label>
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    placeholder="Your Email"
+                    autoComplete="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="your_pass">
+                    <i className="zmdi zmdi-lock"></i>
+                  </label>
+                  <input
+                    type="password"
+                    name="your_pass"
+                    id="your_pass"
+                    placeholder="Password"
+                    autoComplete="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <label
+                  htmlFor="remember-me"
+                  className="forget-pass"
+                  onClick={resetPass}
+                >
+                  Forget Password?
+                </label>
+                <div className=" form-button">
+                  <input
+                    style={{
+                      height: "50px",
+                      fontWeight: "bold",
+                      fontSize: "15px",
+                      marginLeft: "65px",
+                      padding: "10px 30px",
+                    }}
+                    type="submit"
+                    name="signin"
+                    id="signin"
+                    className="form-submit"
+                    value="Log in"
+                    onClick={signIn}
+                  />
+                </div>
+              </form>
+
+              <button
+                className="login-with-google-btn"
+                type="submit"
+                onClick={signInWithGoogle}
+              >
+                Continue with Google
+              </button>
+              <br />
+              <br />
+              <br />
+              <hr />
+
+              <div className="social-login">
+                <a
+                  href="/signup"
+                  style={{ textDecoration: "none", marginTop: "-5px" }}
+                >
+                  <input
+                    style={{
+                      marginTop: "0px",
+                      marginLeft: "25px",
+                      color: "white",
+                      fontSize: "15px",
+                      backgroundColor: "#42b72a",
+                      border: "1px solid transparent",
+                      padding: "10px 15px",
+                      fontWeight: "bold",
+                      width: "80%",
+                    }}
+                    type="text"
+                    readOnly
+                    className="form-submit"
+                    value="   Create a new account"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <hr />
+      <hr />
+    </div>
+  );
+}
+
+export default Login;
